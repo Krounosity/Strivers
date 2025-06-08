888e11f6e989e5a3e267c92a95a0d281975557f8
Varnan Rathod <varnanrathod6@gmail.com>
2025-06-09 00:16:19 +0530

    Rearrange array elements to alternate signs question completed

---
+++ DSA/Bit_Manipulation/Alternate_Signs.java
@@ -0,0 +1,18 @@
+class Alternate_Signs {
+    public int[] rearrangeArray(int[] nums) {
+        ArrayList<Integer> positives = new ArrayList<>();
+        ArrayList<Integer> negatives = new ArrayList<>();
+
+        for(int n : nums){
+            if(n > 0) positives.add(n);
+            else negatives.add(n);
+        }
+
+        for(int i=0; i<positives.size(); i++){
+            nums[2*i] = positives.get(i);
+            nums[2*i+1] = negatives.get(i);
+        }
+
+        return nums;
+    }
+}
