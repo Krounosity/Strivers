9e2a13156b38c8b0af6f871c64c9c9bd3b218eb0
Varnan Rathod <varnanrathod6@gmail.com>
2025-06-10 11:22:58 +0530

    Remove outer parantheses question completed DSA strings easy

---
+++ DSA/Strings/Remove_Outer_Parantheses.java
@@ -0,0 +1,23 @@
+package DSA.Strings;
+
+class Remove_Outer_Parentheses {
+    public String removeOuterParentheses(String s) {
+        StringBuilder res = new StringBuilder();
+
+        int depth = 0;
+        for(char ch : s.toCharArray()){
+            if(ch == '('){
+                if(depth > 0){
+                    res.append(ch);
+                }
+                depth++;
+            } else {
+                if(depth > 1){
+                    res.append(ch);
+                }
+                depth--;
+            }
+        }
+        return res.toString();
+    }
+}
