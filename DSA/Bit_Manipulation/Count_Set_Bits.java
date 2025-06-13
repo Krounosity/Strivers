a2edd31257f281c3f61149a1786f0cbd7ab2baff
Varnan Rathod <varnanrathod6@gmail.com>
2025-06-13 19:20:21 +0530

    Corrected comment

--- DSA/Bit_Manipulation/Count_Set_Bits.java
+++ DSA/Bit_Manipulation/Count_Set_Bits.java
@@ -1,6 +1,6 @@
 /* While the number does not go down to 0,
  * If the least significant bit is 1, increase resultant count
- * Left shift the number by 1 bit.
+ * Right shift the number by 1 bit.
  */
 
 class Count_Set_Bits{
