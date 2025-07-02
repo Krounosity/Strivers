082403df6e330e0e7ddd181b28b1b92683941a4b
Varnan Rathod <varnanrathod6@gmail.com>
2025-07-02 22:40:34 +0530

    Reverse words in a string completed DSA Strings lec 2

---
+++ DSA/Strings/Lec2/ReverseWords.java
@@ -0,0 +1,13 @@
+class ReverseWords {
+    public String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+
+        for(int i=0; i<words.length/2; i++){
+            String temp = words[i];
+            words[i] = words[words.length - i - 1];
+            words[words.length - i - 1] = temp;
+        }
+
+        return String.join(" ",words);
+    }
+}
